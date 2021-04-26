de395056a90e7139d5efec7491e83baba57cd571
Max <max.lepan@laposte.net>
2021-04-26 17:12:56 +0200

    good commit

---
+++ scripts/carrousel.js
@@ -0,0 +1,20 @@
+
+/*new Splide( '.splide', {
+    type    : 'loop',
+    perPage: 4,
+    perMove: 1,
+    autoplay: true,
+
+} ).mount();*/
+
+new Splide( '.splide' , {
+    type: "loop",
+    perPage: 4,
+    perMove: 1,
+    autoWidth: true,
+    autoHeight: true,
+    arrows: false,
+    autoplay: true,
+    interval: 5000,
+
+} ).mount();
---
+++ scripts/circle.js
@@ -0,0 +1,6 @@
+let circle = document.querySelector("#my-projects-circle");
+let carousel = document.querySelector("#carousel");
+
+circle.addEventListener("click", () => {
+    carousel.scrollIntoView({behavior: "smooth"});
+})
