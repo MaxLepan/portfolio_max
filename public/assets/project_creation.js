c69e03f21f351a04dc5b08c6100c15c04b5773ec
MaxLepan <max.lepan@laposte.net>
2021-05-06 11:58:56 +0200

    changed APILocation

--- public/assets/project_creation.js
+++ public/assets/project_creation.js
@@ -14,26 +14,6 @@ export const singleProjectCreation = (getProject, /*link,*/ allowTag = true) => 
         let projectId = parseInt(params.get('projectId'))
         const projectOnPage = data.findIndex(project => project.id.toString() === projectId.toString())
 
-        /*const currentProjectIndex = data.findIndex(project => project.id === projectId)
-
-
-        let nextProjectIndex = currentProjectIndex + 1
-        let previousProjectIndex = currentProjectIndex - 1
-
-
-        if (previousProjectIndex === -1) {
-            previousProjectIndex = data.length - 1
-        }
-
-        if (nextProjectIndex === data.length) {
-            nextProjectIndex = 0
-        }
-
-
-        let nextProjectUrl = `${location}/${link}/pages/single_project.html?projectId=${data[nextProjectIndex].id}`
-        let previousProjectUrl = `${location}/${link}/pages/single_project.html?projectId=${data[previousProjectIndex].id}`
-*/
-
         const singleProject = document.getElementById("project-wrapper")
 
 
@@ -61,10 +41,7 @@ export const singleProjectCreation = (getProject, /*link,*/ allowTag = true) => 
                     <div id="skills-carousel"></div>
                 </div>
                 <div id="prj-img-container">
-                <!--IMAGES SECONDAIRES-->
-                    <!--<div id="prj-img1" class="prj-imgs">
-                        <img src="$singleProjectImg}" height="100%" class="prj-img-wrapper">
-                    </div>-->
+
                 </div>
         `
 
@@ -88,24 +65,6 @@ export const singleProjectCreation = (getProject, /*link,*/ allowTag = true) => 
             }
 
         })
-
-        /*if (allowTag) {
-            document.getElementById("texts").innerHTML += `<div class="tags-row"></div>`
-            const tagsRow = document.querySelector('.tags-row')
-            const tags = projectOnPage.tags
-
-            if (tags.indexOf("nothomepage") !== -1) {
-                const indexTag = tags.indexOf("nothomepage")
-                tags.splice(indexTag, 1)
-            }
-
-            tags.forEach((tag, id) => {
-                if (id < 3) {
-                    tagsRow.innerHTML += `<p class="tag">${tag}</p>`
-                }
-            })
-
-        }*/
 
     })
 }
